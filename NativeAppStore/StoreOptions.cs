4a0c5e63d08131315227a96ce9eeda32aa1fbf0a
zyknow <1047606598@qq.com>
2023-08-21 11:36:52 +0800

    chore: update

--- NativeAppStore/StoreOptions.cs
+++ NativeAppStore/StoreOptions.cs
@@ -2,9 +2,18 @@
 
 public class StoreOptions
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public string EncryptionKey { get; set; } = "NativeAppStore";
 
+    /// <summary>
+    /// 
+    /// </summary>
     public string DefaultRootFolderPath { get; set; } = "AppStores";
 
+    /// <summary>
+    /// when true, store will be first get to load,if this is false, you need manual to invoke LoadStore method
+    /// </summary>
     public bool EnabledCreatorStoreLoad { get; set; }
 }
